ab6ed8b2fff95fb89049836041ddf43be82d7c1c
Maxim Kazakov <m.c.kazakov.developer@gmail.com>
2023-05-13 16:54:42 +0300

    Add test

--- src/test/java/ru/otus/pattern/adapter/program2/StartApp2Test.java
+++ src/test/java/ru/otus/pattern/adapter/program2/StartApp2Test.java
@@ -2,7 +2,12 @@ package ru.otus.pattern.adapter.program2;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StartApp2Test {
 
@@ -11,6 +16,14 @@ class StartApp2Test {
     @Test
     public void startProgram2TestShouldSuccess() {
         assertDoesNotThrow(() -> startApp2.startProgram2());
+    }
+
+    @Test
+    public void testFileOutputOfGenerateMatrixExists() {
+        String fileName = "outputOfGenerateMatrix.txt";
+        Path filePath = Paths.get(fileName);
+
+        assertTrue(Files.exists(filePath));
     }
 
 }
